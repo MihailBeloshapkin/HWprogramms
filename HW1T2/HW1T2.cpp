1beb8e1bfdd46778f3857b47754aab86b4eef555
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2019-11-03 16:07:47 +0300

    add solutions

---
+++ HW1T2/HW1T2.cpp
@@ -0,0 +1,110 @@
+﻿#include "pch.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath> 
+
+int dipRand(int minValue = 2, int maxValue = 15)
+{
+	return rand() % (maxValue - minValue + 1) + minValue;
+}
+
+int divide(int dividend, int divider)
+{
+	int answer = 0; 
+	int modDividend = fabs(dividend);
+	int modDivider = fabs(divider);
+	int quotient = modDivider;
+	bool isBalance = true;
+	while (quotient <= modDividend)
+	{
+		if (quotient == modDividend)
+		{
+			isBalance = false;
+		}
+		answer++;
+		quotient += modDivider;
+	}
+    
+	if ((dividend >= 0 && divider > 0) || (dividend <= 0 && divider < 0))
+	{
+		return answer;
+	}
+	if (dividend >= 0 && divider < 0)
+	{
+		return -answer;
+	}
+	if (dividend < 0 && divider > 0)
+	{
+		if (!isBalance)
+		{
+			return -answer;
+		}
+		return -(answer + 1);
+	}
+	return 0;
+}
+
+bool test()
+{
+	bool isCorrectly = true;
+	int testDividend = dipRand(10, 20);
+	int testDivider = dipRand(2, 9);
+	if (divide(testDividend, testDivider) != (int)((float)testDividend / testDivider))
+	{
+		printf("Incorrect work with random values\n");
+		isCorrectly = false;
+	}
+	if (divide(-7, 3) != -3)
+	{
+		printf("Incorrect work with (-7, 3) arguments\n");
+		isCorrectly = false;
+	}
+	if (divide(7, -3) != -2)
+	{
+		printf("Incorrect work with (7, -3) arguments\n");
+		isCorrectly = false;
+	}
+	if (divide(10, 3) != 3)
+	{
+		printf("Incorrect work with (10, 3) arguments\n");
+		isCorrectly = false;
+	}
+	if (divide(-10, -3) != 3)
+	{
+		printf("Incorrect work with (-10, -3) arguments\n");
+		isCorrectly = false;
+	}
+	if (divide(9, 3) != 3)
+	{
+		printf("Incorrect work with (9, 3) arguments\n");
+		isCorrectly = false;
+	}
+	if (divide(9, -3) != -3)
+	{
+		printf("Incorrect work with (9, -3) arguments\n");
+		isCorrectly = false;
+	}
+	return isCorrectly;
+}
+
+int main() 
+{ 
+	if (!test())
+	{
+		printf("Incorrect work divide function\n");
+		return -1;
+	}
+	int dividend = 0; 
+	int divider = 0; 
+	printf("Input dividend:\n"); 
+	scanf_s("%d", &dividend); 
+	printf("Input divider:\n");
+	scanf_s("%d", &divider); 
+	if (divider == 0)
+	{
+		printf("Incorrect value of divider\n");
+		return 0;
+	}
+	printf("Answer is: %i\n", divide(dividend, divider));
+	return 0;
+}
---
+++ HW1T3/HW1T3.cpp
@@ -0,0 +1,48 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+
+
+
+int main()
+{
+	int tmp, counter = 0;
+	const int m = 3, n = 4, N = m + n;
+	int Array[N];
+	printf("Input array of integer numbers:\n");
+    for (int i = 0; i < N; i++)
+		scanf_s("%i", &Array[i]);
+
+	for (int i = 0; i < m; i++) {
+		tmp = Array[0];
+		for (int j = 0; j < m - i; j++) {
+			Array[j] = Array[j + 1];
+		}
+		Array[m - 1 - i] = tmp;
+	}
+	for (int i = m; i < N; i++) {
+		tmp = Array[m];
+		for (int j = m; j < N - counter; j++) {
+			Array[j] = Array[j + 1];
+		}
+		Array[N - 1 - counter] = tmp;
+		counter++;
+	}
+	for (int i = 0; i < N; i++) {
+		tmp = Array[0];
+		for (int j = 0; j < N - i; j++) {
+			Array[j] = Array[j + 1];
+		}
+		Array[N - 1 - i] = tmp;
+	}
+
+	
+	for (int i = 0; i < N; i++)
+		printf("%i, ", Array[i]);
+
+		
+}
+
