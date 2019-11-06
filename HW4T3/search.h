c9f5fb658d5f03bd592b6f9df63dcd235217a46c
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2019-11-07 00:37:34 +0300

    dekete

--- HW4T3/search.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "notebook.h"
-
-// Search phone using name.
-int searchPhone(Note *data, char *name, int size);
-
-// Search name using phone.
-int searchName(Note *data, char *phone, int size);
